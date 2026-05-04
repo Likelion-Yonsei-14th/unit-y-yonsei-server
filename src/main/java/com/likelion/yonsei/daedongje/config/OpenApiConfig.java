87fc20b0aee52eae8f34989a398edcf86673ac7e
우태호 <ehho0916@yonsei.ac.kr>
2026-05-04 19:07:17 +0900

    refactor: OpenAPI 버전을 BuildProperties에서 주입하도록 변경
    리뷰 의견 반영. 하드코딩된 v0.0.1이 Gradle 프로젝트 버전과 드리프트할 위험을 제거.
    springBoot.buildInfo()로 META-INF/build-info.properties를 생성해 BuildProperties 빈을 자동 등록하고,
    OpenAPI Info.version에 주입한다. build-info가 없는 컨텍스트(일부 테스트 등) 대비해 Optional로 받음.

--- src/main/java/com/likelion/yonsei/daedongje/config/OpenApiConfig.java
+++ src/main/java/com/likelion/yonsei/daedongje/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.likelion.yonsei.daedongje.config;
 
+import java.util.Optional;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
 	@Bean
-	public OpenAPI daedongjeOpenAPI() {
+	public OpenAPI daedongjeOpenAPI(Optional<BuildProperties> buildProperties) {
+		String version = buildProperties.map(BuildProperties::getVersion).orElse("unknown");
 		return new OpenAPI()
 				.info(new Info()
 						.title("무악대동제 2026 API")
 						.description("연세대학교 개교 141주년 무악대동제 2026 백엔드 API 문서")
-						.version("v0.0.1"));
+						.version(version));
 	}
 }
